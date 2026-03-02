da8f0aee40d82db472466d9e09548d06addf2d7d
Edi Hamiti <edi.hamiti.etu@univ-lille.fr>
2026-03-02 15:03:56 +0100

    ajout de la table WasteType au data.sql

--- res/data.sql
+++ res/data.sql
@@ -1,3 +1,42 @@
+DROP TABLE IF EXISTS WasteType;
+
+CREATE TABLE WasteType(
+                          id SERIAL,
+                          nom TEXT,
+                          pointsPerKilo INT
+);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('vert', 10);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('gravats', 12);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('métaux', 30);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('métaux', 30);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('bois', 20);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('plastiques', 9);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('textiles', 11);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('électronique', 22);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('encombrants', 14);
+
+INSERT INTO WasteType(nom, pointsPerKilo)
+VALUES ('cartons', 7);
+
+
 DROP TABLE IF EXISTS CollectionPoint;
 
 CREATE TABLE CollectionPoint(
